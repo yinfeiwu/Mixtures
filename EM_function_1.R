f65d9272f991094c6301d6fe9c1618215f3cc40b
Audrey <audrey.hendricks@ucdenver.edu>
2018-03-20 14:23:28 -0600

    Update EM_function_1.R

--- EM_function_1.R
+++ EM_function_1.R
@@ -13,9 +13,14 @@ setwd("/home/projects/mixtures")
 
 person="Kendra" ## make a directory with your name - make sure you use your name when running the code so you are not overwriting others work
 
+
+#############  ExAC data  ###########
+exac<-read.table("ExAC/all.common_added_cols_v2.txt", sep="\t", header=T, as.is=T, fill=T)
+
+
 ########  sim parameters  ##########
 Ntot=10000  ##total number of simulated samples
-N_AFR=5000
+N_pop1=5000
 MAF_thresh=0.05
 pop_names<-c("AFR", "NFE") ##c("AFR","AMR", "EAS","FIN","OTH","SAS", "NFE") ##
 k=length(pop_names)
@@ -23,17 +28,14 @@ pops<-ifelse(k==2, "AFR_NFE", "ALL") ##"ALL"  ##AFR_NFE"  ##
 pi_start<-rep(1/k, k)  ##c(.5, .5) ##c(.9, .1)
 threshold=0.0001
 
-#############  ExAC data  ###########
-exac<-read.table("ExAC/all.common_added_cols_v2.txt", sep="\t", header=T, as.is=T, fill=T)
-
 
 ############# simulating NFE and African mixture ##########
-x.obs.NFE<- as.data.frame(exac[,c("AC_hom_NFE", "AC_het_NFE", "AC_homref_NFE")])
-x.obs.AFR<- as.data.frame(exac[,c("AC_hom_AFR", "AC_het_AFR", "AC_homref_AFR")])
-x.sim.NFE<-t(apply(x.obs.NFE, 1, function(x){rmultinom(1, (Ntot-N_AFR), prob=(x))}))
-x.sim.AFR<-t(apply(x.obs.AFR, 1, function(x){rmultinom(1, N_AFR, prob=(x))}))
+x.obs.pop2<- as.data.frame(exac[,paste(c("AC_hom_", "AC_het_", "AC_homref_"), pop_names[2], sep="")])
+x.obs.pop1<- as.data.frame(exac[,paste(c("AC_hom_", "AC_het_", "AC_homref_"), pop_names[1], sep="")])
+x.sim.pop2<-t(apply(x.obs.pop2, 1, function(x){rmultinom(1, (Ntot-N_pop1), prob=(x))}))
+x.sim.pop1<-t(apply(x.obs.pop1, 1, function(x){rmultinom(1, N_pop1, prob=(x))}))
 
-x.touse.a<-x.sim.NFE+x.sim.AFR
+x.touse.a<-x.sim.pop2+x.sim.pop1
 x.touse<-as.data.frame(x.touse.a)
 x.touse$AF<-(2*x.touse[,1]+x.touse[,2])/(2*Ntot)
 x<-x.touse[x.touse$AF>MAF_thresh & x.touse$AF<(1-MAF_thresh),1:3]
@@ -41,7 +43,7 @@ x<-x.touse[x.touse$AF>MAF_thresh & x.touse$AF<(1-MAF_thresh),1:3]
 ###########  EM1 FUNCTION  #############
 N=nrow(x) # number of markers
 
-if(pops=="AFR_NFE"){p<-exac[which(x.touse$AF>MAF_thresh & x.touse$AF<(1-MAF_thresh)),c("AC_hom_AFR", "AC_het_AFR", "AC_homref_AFR","AC_hom_NFE", "AC_het_NFE", "AC_homref_NFE")]}
+if(pops=="AFR_NFE"){p<-exac[which(x.touse$AF>MAF_thresh & x.touse$AF<(1-MAF_thresh)),paste(c("AC_hom_", "AC_het_", "AC_homref_"), rep(pop_names, each=3), sep="")]}
 if(pops=="ALL"){p<-exac[which(x.touse$AF>MAF_thresh & x.touse$AF<(1-MAF_thresh)),c("AC_hom_AFR", "AC_het_AFR", "AC_homref_AFR","AC_hom_AMR", "AC_het_AMR", "AC_homref_AMR","AC_hom_EAS", "AC_het_EAS", "AC_homref_EAS", "AC_hom_FIN", "AC_het_FIN", "AC_homref_FIN", "AC_hom_OTH", "AC_het_OTH", "AC_homref_OTH", "AC_hom_SAS", "AC_het_SAS", "AC_homref_SAS", "AC_hom_NFE", "AC_het_NFE", "AC_homref_NFE")]}
 
 ##p<-apply(p.a, 2, function(x){ifelse(is.na(x),0,x)})  ##assuming missing in ExAC is AF=0
@@ -73,7 +75,7 @@ while( thresh_check > threshold){
     pi_out_median<-rbind(pi_out_median, pi_median)
     iter=iter+1
     thresh_check<-sum(abs(pi-pi_new))
-    if(round(iter/10)==(iter/10)){write.table(gamma_tmp, paste(person, "/gamma_values/pops", pops, "AFR", N_AFR, "_NFE", (N-N_AFR), "_start", paste(pi_start, collapse="_"), "_iter", iter, ".txt",sep=""), row.names=F, col.names=F, quote=F, sep="\t")}
+    if(round(iter/10)==(iter/10)){write.table(gamma_tmp, paste(person, "/gamma_values/pops", pops, pop_names[1], N_pop1, "_", pop_names[2], (N-N_pop1), "_start", paste(pi_start, collapse="_"), "_iter", iter, ".txt",sep=""), row.names=F, col.names=F, quote=F, sep="\t")}
     print(pi)
     print(nrow(x))
 }
@@ -81,7 +83,7 @@ while( thresh_check > threshold){
 
 pi_out2<-cbind(pi_out, pi_out_median, iter, N)
 
-write.table(pi_out2, paste(person, "/pops", pops, "_", N_AFR, "_", (Ntot-N_AFR), "_start", paste(pi_start, collapse="_"), ".txt",sep=""), row.names=F, col.names=F, quote=F, sep="\t")
+write.table(pi_out2, paste(person, "/pops", pops, "_", N_pop1, "_", (Ntot-N_pop1), "_start", paste(pi_start, collapse="_"), ".txt",sep=""), row.names=F, col.names=F, quote=F, sep="\t")
 
 
 
