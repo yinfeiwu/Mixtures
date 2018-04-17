526373a378ce2b0ae8798a58f22abee7318fc419
Audrey <audrey.hendricks@ucdenver.edu>
2018-04-17 13:22:28 -0600

    Update loop_submit.R

--- loop_submit.R
+++ loop_submit.R
@@ -25,20 +25,21 @@ mkdir("loop")
 pop_names_loop<-data.frame(pop1="AFR", pop2=c("AMR", "EAS","FIN","OTH","SAS", "NFE"))
 
 
-########  other sim parameters  ##########
+########  sim parameters  ##########
 Ntot=10000  ##total number of simulated samples
 N_pop1=5000
 MAF_thresh=0.05
-pi_start<-rep(1/k, k)  ##c(.5, .5) ##c(.9, .1)
+##k=length(pop_names)
+pi_start<-c(.5, .5)  ##c(.5, .5) ##c(.9, .1) rep(1/k, k) 
 ##pop_names<-c("AFR", "NFE") ##c("AFR","AMR", "EAS","FIN","OTH","SAS", "NFE") ##
 threshold=0.0001
 
 tosubmit<-c()
 for(i in 1:nrow(pop_names_loop)){
-    ###FIX###
-   tmp<-paste("EM1_loop_Ntot", Ntot, "_Npop1", N_pop1, "_MAFthresh", MAF_thresh, "_popnames", paste(t(pop_names_loop[i,]), collapse=""), "_threshold", threshold, sep="")
     
-    write(c(paste("Ntot = ", Ntot, sep=""), paste("N_pop1 = ", N_pop1, sep=""), paste("MAF_thresh = ", MAF_thresh, sep=""), paste("pop_names = c('", paste(t(pop_names_loop[i,]), collapse="', '"), "')", sep=""), paste("threshold = ", threshold, sep="")), file=paste("/home/projects/mixtures/loop/", tmp, ".R", sep=""))
+   tmp<-paste("EM1_loop_Ntot", Ntot, "_Npop1", N_pop1, "_MAFthresh", MAF_thresh, "_pi_start", paste(t(pi_start), collapse=""), "_popnames", paste(t(pop_names_loop[i,]), collapse=""), "_threshold", threshold, sep="")
+    
+    write(c(paste("Ntot = ", Ntot, sep=""), paste("N_pop1 = ", N_pop1, sep=""), paste("MAF_thresh = ", MAF_thresh, sep=""), paste("pi_start = c(", paste(t(pi_start), collapse=","), ")", sep=""),paste("pop_names = c('", paste(t(pop_names_loop[i,]), collapse="', '"), "')", sep=""), paste("threshold = ", threshold, sep="")), file=paste("/home/projects/mixtures/loop/", tmp, ".R", sep=""))
     write("source('/home/projects/mixtures/EM_function_1_forlooping.R')", file=paste("/home/projects/mixtures/loop/", tmp, ".R", sep=""), append=T)
     
     write(c("#$ -cwd",paste("#$ -o /home/projects/mixtures/loop/", tmp, ".log", sep=""),paste("#$ -o /home/projects/mixtures/loop/", tmp, ".err", sep=""),"#$ -S /bin/bash", "", paste("Rscript /home/projects/mixtures/loop/", tmp, ".R", sep="")),file=paste("/home/projects/mixtures/loop/", tmp, ".sh", sep=""))
