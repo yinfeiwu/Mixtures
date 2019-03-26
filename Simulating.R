7d066515c446c35daa0e53ff1e492ae2ee7ad163
GregoryMatesi <44989448+GregoryMatesi@users.noreply.github.com>
2019-03-26 14:58:52 -0600

    Update Simulating.R

--- Simulating.R
+++ Simulating.R
@@ -11,27 +11,25 @@
 # Thanks to Ian for "snpsampgen.R" and "SLSQPmixturesR.R." And to Jordan for "HA_script.py."
 
 total.c <- read.table("/nfs/storage/math/gross-s2/projects/mixtures/team_members/current_team/Greg/5ancfinalsimdat.txt")
-#install.packages("nloptr")    # Needs to be installed on the server. It contains our SLSQP R function.
-#install.packages("reticulate")
+
 library("nloptr")
 library("reticulate")         # Reticulate is already installed on the server. It links Python with R.
 
 source("/nfs/storage/math/gross-s2/projects/mixtures/team_members/current_team/Greg/snpsampgen.R")
 source("/nfs/storage/math/gross-s2/projects/mixtures/team_members/current_team/Greg/SLSQPmixturesR.R")
 source_python("/nfs/storage/math/gross-s2/projects/mixtures/team_members/current_team/Greg/HA_script.py")    # source_Python() from the "reticulate" package.
-# push to Audrey
+
 
 START <- Sys.time()
 teamMember <- "Greg"              # Put your name here. Please make sure it matches the name of your folder in math-compute.
-# setwd(pasteO("/nfs/storage/math/gr0ss-s2/projects/mixtures/team_members/current_team" , teamMember))
-# Write the file to your personal folder in the math-compute server.
+interval1 <- 0.01                 # Change this to the interval you are working on
+interval2 <- 0.05                 # 
 
-numberSims <- 3                        # Start with 10. then try 100 or 1000.
-k <-5                                  # Keep at 5. This number is input for simulating the sampling population.
+numberSims <- 10                        # Start with 10. then try 100 or 1000.
+k <- 5                                 # Keep at 5. This number is input for simulating the sampling population.
 numAnc <- 2                            # Set by the user. This number is only for recording in the spreadsheet.
-interval <- "(0.01_0.05)"              # Change your interval
-ancestry1 <- "eur_MAF"                 # Name the ancestries you are working with
-ancestry2 <- "afr_MAF"                 #
+ancestry1 <- "afr_MAF"                 # Name the ancestries you are working with
+ancestry2 <- "NA"                 #
 
 guess1 <- 1/k                          # Generally leave this along for now.           
 
@@ -44,38 +42,34 @@ for (i in 1:numberSims){
     seed <- Sys.time() # Save the seed used in the uniform draw and for sampling population.
     
     set.seed(seed)
-    eurfrac <- runif(1, 0.01, 0.05)    # Uniform random from user chosen interval.
-    afrfrac <- 1 - eurfrac             
-    easfrac <- 0
+    afrfrac <- runif(1, interval1, interval2) # Uniform random from user chosen interval.
+    easfrac <- 1 - afrfrac
     sasfrac <- 0
+    eurfrac <- 0
     namfrac <- 0
     
     
     set.seed(seed)
-    A <- snpsampgen(k, "eur_MAF" , "afr_MAF", "eas_MAF", "sas_MAF", "nam_MAF", eurfrac, afrfrac, easfrac, sasfrac, namfrac)    
-    # Simulating a sample pop with new SNPs.
+    A <- snpsampgen(k, "afr_MAF", "eas_MAF", "sas_MAF", "eur_MAF" , "nam_MAF", afrfrac, easfrac, sasfrac, eurfrac, namfrac)    
+    # Simulating a sample population with a new set of 100,00 SNPs.
     
     # Calling the SLSQPmixtures function from SLSQPmixturesR.R
-    Python_ <- print(SLSQPmixtures(A, k))                              # stores pi-values, iters, time.
+    Python_ <- print(SLSQPmixtures(A, k))                              # Returns pi-values, iterations, time.
     
     # Calling the HA function from HA_script.py                      
-    af <- cbind(A$AF)                                                  # Total allele frequency vector
-    A <- cbind(A$eur_MAF, A$afr_MAF, A$eas_MAF, A$sas_MAF, A$nam_MAF)  # CHANGEME: cbind(A$afr_MAF, A$CEU_MAF) etc
+    af <- cbind(A$AF)                                                  # Total allele frequency vector.
+    A <- cbind(A$afr_MAF, A$eas_MAF, A$sas_MAF, A$eur_MAF, A$nam_MAF)  # All global populations used.
     
     guess <- rbind(guess1, guess1, guess1, guess1, guess1)             # kx1 vector of starting guesses.
-    HA_ <- print(HA(A, af, guess))                                     # Stores pi-values, iters, time.
+    HA_ <- print(HA(A, af, guess))                                     # Returns pi-values, iterations, time.
     
-    output <- rbind(output, c(HA_[1], HA_[2], HA_[3], HA_[4], HA_[5], HA_[6], HA_[7], Python_[1], Python_[2], Python_[3], Python_[4], Python_[5], Python_[6], Python_[7], seed, teamMember, eurfrac, afrfrac, easfrac, sasfrac, namfrac))
+    output <- rbind(output, c(HA_[1], HA_[2], HA_[3], HA_[4], HA_[5], HA_[6], HA_[7], Python_[1], Python_[2], Python_[3], Python_[4], Python_[5], Python_[6], Python_[7], seed, teamMember, afrfrac, easfrac, sasfrac, eurfrac, namfrac, eurfrac - as.numeric(HA_[1]), afrfrac - as.numeric(HA_[2]), easfrac - as.numeric(HA_[3]), sasfrac - as.numeric(HA_[4]), namfrac - as.numeric(HA_[5]), eurfrac - as.numeric(Python_[1]), afrfrac - as.numeric(Python_[2]), easfrac - as.numeric(Python_[3]), sasfrac - as.numeric(Python_[4]), namfrac - as.numeric(Python_[5])))
     print(i)
     print(Sys.time() - START)
 }
-colnames(output) <- c("R.eur", "R.afr", "R.eas", "R.sas", "R.nam",  "R.iters", "R.time","Python.eur", "Python.afr", "Python.eas", "Python.sas", "Python.nam", "Python.iters", "Python.time", "seed", "Team.memeber", "True.eur", "True.afr", "True.eas", "True.sas", "True.nam")
+colnames(output) <- c("R.afr", "R.eas", "R.sas","R.eur", "R.nam",  "R.iters", "R.time", "Python.afr", "Python.eas", "Python.sas", "Python.eur", "Python.nam", "Python.iters", "Python.time", "seed", "Team.memeber", "True.afr", "True.eas", "True.sas", "True.eur", "True.nam", "accuracy.HA.afr", "accuracy.HA.eas", "accuracy.HA.sas", "accuracy.HA.eur", "accuracy.HA.nam", "accuracy.Python.afr", "accuracy.Python.eas", "accuracy.Python.sas", "accuracy.Python.eur", "accuracy.Python.nam")
 
-# write output to personal file
-# past0("/nfs/storage/math/gr0ss-s2/projects/mixtures/team_members/current_team/", teamMember, n, interval, ancestry1, "_", ancestry2)
-# past0("/home/jovyan, teamMember, n, interval, ancestry1, "_", ancestry2)
-#write.csv(output, file = paste0("/home/jovyan/", teamMember, "/", numAnc, "_", interval, "_", ancestry1, "_", ancestry2))
-write.csv(output, file = paste0("/nfs/storage/math/gross-s2/projects/mixtures/team_members/current_team/", teamMember, "/", numAnc, "_", interval, "_", ancestry1, "_", ancestry2, ".csv"))
+write.csv(output, file = paste0("/nfs/storage/math/gross-s2/projects/mixtures/team_members/current_team/", teamMember, "/", ancestry1, "_", interval1, "-", interval2, "_", ancestry2,  ".csv"))
 
 
 END <- Sys.time()
